89e373ae7d19669a3dcaf2a4f2bc026da1e3bf24
Povolnik Gergő <povolnikgergo@gmail.com>
2022-04-06 13:20:51 +0200

    feladat

---
+++ kodol.py
@@ -0,0 +1,41 @@
+#1. Feladat - Povolnik
+
+
+
+
+
+#2. Feladat - Tóth Ádám
+
+
+
+
+
+
+#3. Feladat - Horgos Hektor
+
+
+
+
+
+
+#4. Feladat - Horgos Hektor
+
+
+
+
+
+
+
+
+#5. Feladat - Tóth Ádám
+
+
+
+
+#6. Feladat - Povolnik Gergő 
+
+
+
+
+
+#7. Feladat - Tóth Ádám
